d75df660e6f00a8ca46d5aa97a496bbb65814800
tauan-longaretti <tauanlongaretti@gmail.com>
2019-11-22 22:07:08 -0500

    fetched luke skywalker from api and rendered to the screen

--- starwars/src/components/SWAPI/Character.js
+++ starwars/src/components/SWAPI/Character.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
+// import './App.css';
+import CharacterCard from "./CharacterCard"
 
 const Character = () => {
   const [setCharacter, setCharacterState] = useState([]);
   useEffect(() => {
-      axios.get()
-  })
-}
+      axios.get("https://swapi.co/api/people/1/")
+      .then(response => {
+          setCharacterState(response.data)
+          console.log(response);
+      })
+      .catch (error => {console.log(error);
+      })
+  },[]);
+
+  return (
+    <div>
+      <CharacterCard
+      name = {setCharacter.name}
+      gender = {setCharacter.gender}
+      height = {setCharacter.height}
+      mass = {setCharacter.mass}           
+      />
+    </div>
+  );
+};
 
 export default Character;
