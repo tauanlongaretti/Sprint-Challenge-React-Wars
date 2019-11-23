74a8805fe2a8a4ad643365d9e265a1ba1b9c24cf
tauan-longaretti <tauanlongaretti@gmail.com>
2019-11-22 21:25:55 -0500

    answered README questions, installed yarn, added axios and concluded the first set of steps for project set up

---
+++ starwars/src/components/SWAPI/Character.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './App.css';
+
+const Character = () => {
+  const [setCharacter, setCharacterState] = useState([]);
+  useEffect(() => {
+      axios.get()
+  })
+}
+
+export default Character;
